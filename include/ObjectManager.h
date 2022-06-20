10d74ae5c232b6f27b5f450941e9dd3d810c149c
Jacob Schaupp <jacob.schaupp@gmail.com>
2022-06-20 13:19:55 -0400

    Update Scale Handling on Object Load

--- include/ObjectManager.h
+++ include/ObjectManager.h
@@ -7,6 +7,7 @@
 #include "SC/Vertex.hpp"
 
 #include <tiny_obj_loader.h>
+#include <glm/glm.hpp>
 
 #include <vector> 
 #include <memory> 
@@ -20,14 +21,17 @@ namespace star{
 
             common::Handle Add(const std::string& pathToFile); 
 
-            common::Handle Add(const std::string& pathToFile, common::Handle texture = common::Handle{ 0 }, common::Handle vertShader = common::Handle{ 0 }, common::Handle fragShader = common::Handle{ 1 });
+            common::Handle Add(const std::string& pathToFile,
+                common::Handle texture = common::Handle{0}, common::Handle vertShader = common::Handle{0}, 
+                common::Handle fragShader = common::Handle{1},
+                glm::vec3 scaleAmt = glm::vec3{ 1.0f, 1.0f, 1.0f });
 
         protected: 
             //load the object from disk 
             void load(const std::string& pathToFile, std::vector<common::Vertex>* vertexList, std::vector<uint32_t>* indiciesList);
 
         private: 
-            common::LogicalObject* create(const std::string& pathToFile, common::Handle texture = common::Handle{ 0 }, common::Handle vertShader = common::Handle{ 0 }, common::Handle fragShader = common::Handle{ 1 });
+            common::LogicalObject* create(const std::string& pathToFile, glm::vec3 scaleAmt = glm::vec3{1.0f, 1.0f, 1.0f}, common::Handle texture = common::Handle{0}, common::Handle vertShader = common::Handle{0}, common::Handle fragShader = common::Handle{1});
 
         };
     }
